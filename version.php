5beb93090680337e038b984cd12b8f658d016712
Prihantoosa <toosa@users.noreply.github.com>
2023-09-03 15:48:38 +0700

    Update version.php
    Sementara requrires moodle version nya di disable dulu, agar bisa di install di versi 3.11

--- version.php
+++ version.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2023071116;
-$plugin->requires  = 2021042100;
+# $plugin->requires  = 2021042100;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.11 onwards';
 $plugin->component = 'local_course_ranking';
